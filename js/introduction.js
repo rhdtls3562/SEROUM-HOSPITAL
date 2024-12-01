ccf41945d953ac43d6046124e360e89991ada2d8
rhdtls3562 <rhdtls3562@naver.com>
2024-12-01 20:27:02 +0900

    모바일 가로스크롤 방지

--- js/introduction.js
+++ js/introduction.js
@@ -57,4 +57,21 @@ $(document).ready(function () {
       behavior: "smooth",
     });
   }
+  $(document).ready(function () {
+    // Show or hide the sticky footer button
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        $(".go-top").fadeIn(200);
+      } else {
+        $(".go-top").fadeOut(200);
+      }
+    });
+
+    // Animate the scroll to top
+    $(".go-top").click(function (event) {
+      event.preventDefault();
+
+      $("html, body").animate({ scrollTop: 0 }, 300);
+    });
+  });
 });
